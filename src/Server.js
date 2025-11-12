dbcd25a81b8620543daa3c667f314ba899fbf1e5
Anshu sharma <156065826+Anshusharma42019@users.noreply.github.com>
2025-11-12 12:02:16 +0530

    update cros

--- src/Server.js
+++ src/Server.js
@@ -8,8 +8,8 @@ const app = express();
 // Middleware
 app.use(cors({
   origin: [
-    "https://regalia-backend.vercel.app",
-    "https://regaliafrontend.vercel.app",
+    "https://budha-backed.vercel.app",
+    "https://budha-f-virid.vercel.app",
     "http://localhost:4000",
     "http://localhost:5173",
     
